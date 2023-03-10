c54f2844c394816d6a559f00290ae635900724b7
leekc3813 <93689662+leekc3813@users.noreply.github.com>
2023-03-10 13:43:51 +0900

    restart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Button, Progress} from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src = {logo} lat= "logo" />
-      <h2>Let's develop management system!</h2>
+    <div>
+      <p> 안녕하세요</p>
+      <Button primary>하이하이</Button>
+      <Progress percent={66}></Progress>
     </div>
-  );
+  )
 }
 
 export default App;
