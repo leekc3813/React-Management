ada1c7be366f49384908d02734d9316e23baca4e
leekc3813 <93689662+leekc3813@users.noreply.github.com>
2023-03-10 16:52:08 +0900

    comments

--- src/comment.js
+++ src/comment.js
@@ -1,37 +1,76 @@
-import React from 'react';
-import { Button, Comment, Form, Header } from 'semantic-ui-react'
-import human from "./people.jpg"
+import React, { useState } from 'react';
+import { Button, Comment, Form, Header } from 'semantic-ui-react';
+import human from "./people.jpg";
 
-class Comments extends React.Component {
-    render() {
-      return (
-        <Comment.Group style = {{marginLeft: "520px"}}>
-        <Header as='h3' dividing>
-          Comments
-        </Header>
-  
+function SingleComment ({ id, author, metadata, text, handleDeleteComment }) {
+    return (
         <Comment>
-          <Comment.Avatar src={human} />
-          <Comment.Content>
-            <Comment.Author as='a'>Matt</Comment.Author>
-            <Comment.Metadata>
-              <div>Today at 5:42PM</div>
-            </Comment.Metadata>
-            <Comment.Text>How artistic!</Comment.Text>
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
+            <Comment.Avatar src={human} />
+            <Comment.Content>
+                <Comment.Author as='a'>{author}</Comment.Author>
+                <Comment.Metadata>
+                    <div>{metadata}</div>
+                </Comment.Metadata>
+                <Comment.Text>{text}</Comment.Text>
+                <Comment.Actions>
+                    <Comment.Action>Reply</Comment.Action>
+                    <Comment.Action onClick={() => handleDeleteComment(id)}>Delete</Comment.Action>
+                </Comment.Actions>
+            </Comment.Content>
         </Comment>
-  
-        <Form reply>
-          <Form.TextArea />
-          <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-        </Form>
-        <br />
-      </Comment.Group>     
-      )
-    }
-  }
+    );
+}
 
-export default Comments
+function Comments() {
+    const [inputContent, setInputContent] = useState("");
+    const [commentsList, setCommentsList] = useState([]);
+    const [commentIdCounter, setCommentIdCounter] = useState(0);
+
+    const handleAddComment = () => {
+        const newComment = { id: commentIdCounter, text: inputContent };
+        setCommentsList(prevCommentsList => [...prevCommentsList, newComment]);
+        setCommentIdCounter(commentIdCounter + 1);
+        setInputContent("");
+    };
+
+    const handleDeleteComment = (id) => {
+        setCommentsList(prevCommentsList => prevCommentsList.filter(comment => comment.id !== id));
+    };
+
+    return (
+        <Comment.Group style={{ marginLeft: "520px" }}>
+            <Header as='h3' dividing>
+                Comments
+            </Header>
+
+            {commentsList.map((comment) => (
+                <SingleComment
+                    key={comment.id}
+                    id={comment.id}
+                    author="Matt"
+                    metadata="Today at 5:42PM"
+                    text={comment.text}
+                    handleDeleteComment={handleDeleteComment}
+                />
+            ))}
+
+            <Form reply>
+                <Form.TextArea
+                    value={inputContent}
+                    placeholder="댓글을 입력하세요!"
+                    onChange={(e) => setInputContent(e.target.value)}
+                />
+                <Button
+                    content='Add Reply'
+                    labelPosition='left'
+                    icon='edit'
+                    primary
+                    onClick={handleAddComment}
+                />
+            </Form>
+            <br />
+        </Comment.Group>
+    );
+}
+
+export default Comments;
